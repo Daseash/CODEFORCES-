593f3d44e2fee3abb8a1f804c8e9bd363f6e569e
DAS-EASH <das.eashwar71@gmail.com>
2026-07-23 22:41:44 +0530

    Add Bear and Big Brother [A] from Codeforces

---
+++ 791/A - Bear and Big Brother/solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int a, b;
+    cin >> a >> b;
+ 
+    int years = 0;
+    while (a <= b) {
+        a *= 3;
+        b *= 2;
+        years++;
+    }
+ 
+    cout << years << "
+";
+ 
+    return 0;
+}
