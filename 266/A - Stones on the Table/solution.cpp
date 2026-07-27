eeb7a891f9d2696f3829ede7ff7bfbcf3cecbabf
DAS-EASH <das.eashwar71@gmail.com>
2026-07-27 22:32:17 +0530

    Add Stones on the Table [A] from Codeforces

---
+++ 266/A - Stones on the Table/solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    
+    int removals = 0;
+    
+    // Start from the second stone and compare it with the previous one
+    for (int i = 1; i < n; ++i) {
+        if (s[i] == s[i - 1]) {
+            removals++;
+        }
+    }
+    
+    cout << removals << endl;
+    return 0;
+}
