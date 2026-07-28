9adbcb2609c330a0c7aa1b3deb2db6c5c4a7a4bc
DAS-EASH <das.eashwar71@gmail.com>
2026-07-28 22:33:05 +0530

    Add Translation [A] from Codeforces

---
+++ 41/A - Translation/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+ 
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    string s, t;
+    cin >> s >> t;
+ 
+    reverse(s.begin(), s.end());
+ 
+    if (s == t) {
+        cout << "YES
+";
+    } else {
+        cout << "NO
+";
+    }
+ 
+    return 0;
+}
