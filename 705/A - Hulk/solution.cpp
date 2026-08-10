90ac93757286c9250a5ab9b3f5b03403a4fb5dcd
DAS-EASH <das.eashwar71@gmail.com>
2026-08-10 22:34:40 +0530

    Add Hulk [A] from Codeforces

---
+++ 705/A - Hulk/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int n;
+    cin >> n;
+ 
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 != 0) {
+            cout << "I hate";
+        } else {
+            cout << "I love";
+        }
+ 
+        if (i == n) {
+            cout << " it
+";
+        } else {
+            cout << " that ";
+        }
+    }
+ 
+    return 0;
+}
